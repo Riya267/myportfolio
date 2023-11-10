28058150f65f4f9b4c732aee5c305279653e40e8
Riya267 <riyacec05@gmail.com>
2023-11-10 14:32:05 +0530

    deployment minor fixes

--- src/utils/toastMessage.ts
+++ src/utils/toastMessage.ts
@@ -1,23 +1,22 @@
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify'
 
-const notify = (message:string, type:string) => {
-    switch (type) {
-        case 'info':
-          toast.info(message, {theme: "colored"});
-          break;
-        case 'error':
-          toast.error(message, {theme: "colored"});
-          break;
-        case 'success':
-          toast.success(message, {theme: "colored"});
-          break;
-        case 'warning':
-          toast.warning(message, {theme: "colored"});
-          break;
-        default:
-          toast(message, {theme: "colored"});
-      }
-};
-
+const notify = (message: string, type: string): void => {
+  switch (type) {
+    case 'info':
+      toast.info(message, { theme: 'colored' })
+      break
+    case 'error':
+      toast.error(message, { theme: 'colored' })
+      break
+    case 'success':
+      toast.success(message, { theme: 'colored' })
+      break
+    case 'warning':
+      toast.warning(message, { theme: 'colored' })
+      break
+    default:
+      toast(message, { theme: 'colored' })
+  }
+}
 
 export default notify
